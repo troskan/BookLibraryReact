22d1423c34b726e2dd42d6e9cb1edffe8ff0d943
Alvin strandberg <Str4ndb3rg@live.se>
2023-09-01 12:44:02 +0200

    Bookpage update, table

--- src/Pages/BookPage.jsx
+++ src/Pages/BookPage.jsx
@@ -22,15 +22,28 @@ const BookPage = () => {
   return (
     <div>
       <h1>Book List</h1>
-      <ul>
-        {books.map((book) => (
-          <li key={book.book}>
-            {" "}
-            {/* Changed from books.id to book.id */}
-            {book.title} - {book.author}
-          </li>
-        ))}
-      </ul>
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="col">Book ID</th>
+            <th className="col">Title</th>
+            <th className="col">Author</th>
+            <th className="col">Available For Loan</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <tr key={book.id}>
+              <th scope="row">{book.bookId}</th>
+
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.isAvailableForLoan ? "Yes" : "No"}</td>
+              <td>View Edit Delete</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
